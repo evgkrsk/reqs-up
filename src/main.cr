1115c44fdc79b40fc2d65640cd840449d76ec9da
Evgenii Tereshkov <evg.krsk@gmail.com>
2021-06-20 15:55:43 +0700

    Add clases skeleton

--- src/main.cr
+++ src/main.cr
@@ -1,14 +1,19 @@
 #!/usr/bin/env crystal
 # -*- mode: crystal; mode: view -*-
 require "option_parser"
+require "./reqs-up"
 
 dryrun = false
-destination = "requirements.yaml"
+source_file = File.new("requirements.yaml")
+destination_file = source_file
 
 OptionParser.parse do |parser|
   parser.banner = "Usage: #{PROGRAM_NAME} [arguments]"
-  parser.on("-n", "--dry-run", "Output result YAML to stdout") { dryrun = true }
-  parser.on("-f FILE", "--file=FILE", "Specifies the FILE instead of ./#{destination}") { |file| destination = file }
+  parser.on("-n", "--dry-run", "Output result YAML to stdout") {
+    dryrun = true
+    destination_file = STDOUT
+  }
+  parser.on("-f FILE", "--file=FILE", "Specifies the FILE instead of ./#{source_file.path}") { |file| source_file = File.new(file) }
   parser.on("-h", "--help", "Show this help") do
     puts parser
     exit
@@ -19,4 +24,9 @@ OptionParser.parse do |parser|
   parser.missing_option do |flag|
     abort("ERROR: #{flag} is missing something.\n#{parser}")
   end
+end
+
+reqs = ReqsUp::Requirements.new(source_file)
+reqs.each do |req|
+  req.update
 end
--- src/reqs-up.cr
+++ src/reqs-up.cr
@@ -4,10 +4,53 @@ require "semantic_version"
 require "yaml"
 
 module ReqsUp
+  enum Versions
+    Latest
+    Minor
+    Major
+    Patch
+  end
+
   # Describes requirements.yaml object
   class Requirements
-    # Initialize requirements object from *file* path
-    def initialize(@file : Path)
+    class_getter number : Int32 = 0 # number of Req-s in Requirements
+    getter reqs : Array(Req)
+
+    # Initialize requirements object from *file* object
+    def initialize(@file : File)
+      @@number += 1
+      # TODO: implement loading from YAML to reqs
+    end
+  end
+
+  # Requirement skeleton
+  abstract class Req
+    property src : String
+    property name : String # TODO: initialize if undefined
+    property version : String = "master"
+    getter scm : String = "git"
+
+    def initialize(src : String, **attrs)
+      # TODO: load attributes
+    end
+
+    # Return all available req versions
+    abstract def versions
+
+    # Update requirement version
+    def update(ver : Versions = Versions::Latest) : Nil
+      # TODO: implement version update
+    end
+  end
+
+  # Requirement implementation for git
+  class GitReq < Req
+    @@scm = "git"
+    @versions : Array(String)
+
+    # fetch git versions
+    def versions
+      Nil # TODO: implement versions fetch
     end
   end
 end

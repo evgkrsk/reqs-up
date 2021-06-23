9ecad134000931298722b397de78532779a7ace2
Evgenii Tereshkov <evg.krsk@gmail.com>
2021-06-23 14:57:27 +0700

    fix15

--- src/main.cr
+++ src/main.cr
@@ -5,6 +5,7 @@ require "./reqs-up"
 require "yaml"
 require "log"
 
+VERSION = {{ `shards version "#{__DIR__}"`.chomp.stringify }}
 dryrun = false
 logbackend = Log::IOBackend.new(STDERR)
 default_source_file = "requirements.yaml"
@@ -13,11 +14,15 @@ source_file = File.new(File::NULL)
 Log.setup_from_env(default_level: :info, backend: logbackend)
 
 OptionParser.parse do |parser|
-  parser.banner = "Usage: #{PROGRAM_NAME} [arguments]"
+  parser.banner = "Usage: #{PROGRAM_NAME} [arguments]\n\nUpdates requirements.yaml file in-place\n"
   parser.on("-n", "--dry-run", "Output result YAML to stdout") { dryrun = true }
   parser.on("-f FILE", "--file=FILE", "Specifies the FILE instead of ./#{default_source_file}") { |file| default_source_file = file }
-  parser.on("-h", "--help", "Show this help") do
+  parser.on("-h", "--help", "Show this help and exit") do
     puts parser
+    exit
+  end
+  parser.on("-v", "--version", "Print version and exit") do
+    puts VERSION
     exit
   end
   parser.invalid_option do |flag|
